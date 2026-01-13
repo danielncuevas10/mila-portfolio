617a06ca6516ea9b02ac8fe5973897e8d8560c8e
Daniel Navarrete Cuevas <danielnavarrete2304@gmail.com>
2026-01-13 15:57:01 +0100

    Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Landing } from "./pages/Landing/Landing";
 import { English } from "./pages/Landing/LandingEnglish";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <Routes>
+        <Analytics />
         <Route path="/" element={<Landing />} />
         <Route path="/english" element={<English />} />
       </Routes>
